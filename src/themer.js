68117409cc8650e902d10d5be1e7206214d7d5c6
dragonfly1033 <42915850+dragonfly1033@users.noreply.github.com>
2024-05-17 22:15:18 +0100

    Update themer.js to fix random background

--- src/themer.js
+++ src/themer.js
@@ -4,7 +4,14 @@ export const DEFAULT_COLOR = '#249cea';
 const DEFAULT_BG = require('./assets/images/background.png');
 
 export let color = localStorage.getItem('color') || DEFAULT_COLOR;
-export let background = getBackground();
+getBackground();
+export let background = localStorage.getItem('background');
+if (window.greeter_comm) {
+    greeter_comm.broadcast({
+        type: "change-background",
+        path: background,
+    })
+}
 
 export function hook(element, rules) {
     const style = element.style;
@@ -32,30 +39,26 @@ export function updateBG(bg) {
     }
 }
 
-export async function backgrounds() {
+export function backgrounds() {
     const folder = greeter_config.branding.background_images_dir ||
                     greeter_config.branding.background_images;
     if (!folder) {
         return [DEFAULT_BG];
     }
 
-    const recDirList = async (dir) => {
+    const recDirList = (dir) => {
         let result = [];
         let dirlist = [];
-        await new Promise((resolve) => {
-            let dirl = theme_utils.dirlist(dir, false, (files) => {
-                dirlist = files;
-                resolve();
-            })
-            if (Array.isArray(dirl)) {
-                dirlist = dirl;
-                resolve();
-            }
+        let dirl = theme_utils.dirlist(dir, false, (files) => {
+            dirlist = files;
         })
+        if (Array.isArray(dirl)) {
+            dirlist = dirl;
+        }
 
         for (const file of dirlist) {
             if (!file.includes('.')) { // I didn't find any good ways to do it
-                result = [...result, ...(await recDirList(file))];
+                result = [...result, ...recDirList(file)];
             } else if (!file.endsWith('.xml') && !file.endsWith('.stw')) { // Gnome and Arch backgrounds have strange files
                 result.push(file);
             }
@@ -63,15 +66,18 @@ export async function backgrounds() {
         return result;
     };
 
-    let result = await recDirList(folder);
+    let result = recDirList(folder);
 
-    return [DEFAULT_BG, ...result];
+    if (result.length === 0) {
+        localStorage.setItem('background', DEFAULT_BG);
+    } else {
+        localStorage.setItem('background', result[Math.floor(Math.random()*result.length)]);
+    }
 }
 
 function getBackground() {
     if(settings.randomizeBG) {
-        const bgs = backgrounds();
-        return bgs[Math.floor(Math.random() * bgs.length)];
+        backgrounds();
     }
 
     return localStorage.getItem('background') || DEFAULT_BG;
